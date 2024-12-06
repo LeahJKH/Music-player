7e9e9c8ade6a762a4fd2687f32e016fb5e11514d
LeahJKH <leahjb06.kodehode@gmail.com>
2024-12-06 09:38:04 +0100

    made code less and more readable

--- js/app.js
+++ js/app.js
@@ -1,61 +1,12 @@
-const soundsFiler = {
-  folder: "audio/",
-  sounds: [
-    {
-      file: "obama.mp3",
-      name: "obama!",
-      key: "x",
-      img: "obamna.jfif",
-    },
-    {
-      file: "sus.mp3",
-      name: "impostor",
-      key: "c",
-      img: "germ.jfif",
-    },
-    {
-      file: "Bonk.wav",
-      name: "Bonkers",
-      key: "b",
-      img: "bonk.png",
-    },
-    {
-      file: "vine-boom.mp3",
-      name: "boom",
-      key: "v",
-      img: "boom.png",
-    },
-    {
-      file: "pluh.mp3",
-      name: "Pluh!!!",
-      key: "n",
-      img: "pluh.png",
-    },
-    {
-      file: "wow.mp3",
-      name: "WooooW",
-      key: "m",
-      img: "Wow.jpg",
-    },
-    {
-      file: "bup.mp3",
-      name: "bup",
-      key: "p",
-      img: "Bup.png",
-    },
-    {
-      file: "Scream.mp3",
-      name: "scream",
-      key: "z",
-      img: "AHHH.jfif",
-    },
-  ],
-};
+import { soundsFiler } from "./module.js"; //imports the object
 
 const soundField = document.querySelector("#sounds-magic");
-const btnImgs = document.querySelector("#showbtnimg");
+const btnImgsDiv = document.querySelector("#show-btn-img-div");
 const inputBtns = document.querySelector("#btn-inputs");
 
+const btnImgs = document.createElement("img")
+
+// this is the main logic too make it all work telling where to find what and parsing the object through
 soundsFiler.sounds.forEach((sound) => {
   const magicSound = btnCreate(sound.name);
 
@@ -64,24 +15,26 @@ soundsFiler.sounds.forEach((sound) => {
 
   magicSound.addEventListener("pointerdown", () => {
     playSound(btnAudio);
-    showImg(sound.img);
+    showImg(sound.img, sound.alt);
   });
   soundField.append(magicSound);
-});
 
-soundsFiler.sounds.forEach((sound) => {
   const inputButton = inputCreate(sound.key, sound.name);
   inputBtns.append(inputButton);
 });
 
+
+
+// This checks which key you press down so that it will display an image and play a sound when the right key is pressed
 document.addEventListener("keydown", (event) => {
-  const soundObj = soundsFiler.sounds.find((sound) => sound.key === event.key);
+  const soundObj = soundsFiler.sounds.find((sound) => sound.key === event.key); //this checks if the key exists in the object
   if (soundObj) {
     playSound(soundObj.audioElement);
     showImg(soundObj.img);
   }
 });
 
+//this plays the sound
 function playSound(audioElement) {
   if (audioElement.paused) {
     audioElement.play();
@@ -91,19 +44,25 @@ function playSound(audioElement) {
   }
 }
 
+//this creates the main buttons that you can click onscreen
 function btnCreate(name) {
   const btnEl = document.createElement("button");
   btnEl.textContent = name;
   return btnEl;
 }
+
+// this dynamically changes the button inputs to that you can show it while still adding too the object
 function inputCreate(input, name) {
   const pMaker = document.createElement("p");
   pMaker.textContent = `${input}: ${name} `;
   return pMaker;
 }
 
-function showImg(img) {
-  const imgFile = "imgs/";
-  btnImgs.src = `${imgFile}${img}`;
+//this sets up image for each time its called
+function showImg(img, alt) {
+  btnImgs.id = "showbtnimg"
+  btnImgs.src = `imgs/${img}`;
+  btnImgs.alt = `${alt}`
   btnImgs.style.display = "flex";
+  btnImgsDiv.appendChild(btnImgs)
 }
---
+++ js/module.js
@@ -0,0 +1,62 @@
+export const soundsFiler = {
+    folder: "audio/",
+    sounds: [
+      {
+        file: "obama.mp3",
+        name: "obama!",
+        key: "x",
+        img: "obamna.webp",
+        alt: "picture of obama wearing a suit looking proud"
+      },
+      {
+        file: "sus.mp3",
+        name: "impostor",
+        key: "c",
+        img: "germ.webp",
+        alt: "picture of jerma distorted smile"
+      },
+      {
+        file: "Bonk.wav",
+        name: "Bonkers",
+        key: "b",
+        img: "bonk.webp",
+        alt: "picture of a baseball bat"
+      },
+      {
+        file: "vine-boom.mp3",
+        name: "boom",
+        key: "v",
+        img: "boom.webp",
+        alt: "an icon of a speaker"
+      },
+      {
+        file: "pluh.mp3",
+        name: "Pluh!!!",
+        key: "n",
+        img: "pluh.webp",
+        alt: "emoji of a person speaking"
+      },
+      {
+        file: "wow.mp3",
+        name: "WooooW",
+        key: "m",
+        img: "Wow.webp",
+        alt: "an anime girl with stars in her eyes looking excited"
+      },
+      {
+        file: "bup.mp3",
+        name: "bup",
+        key: "p",
+        img: "Bup.webp",
+        alt: "model of toad from super mario party for the nintendo 64"
+      },
+      {
+        file: "Scream.mp3",
+        name: "scream",
+        key: "z",
+        img: "AHHH.webp",
+        alt: "picture of a woman falling"
+      },
+    ],
+  };
+  
